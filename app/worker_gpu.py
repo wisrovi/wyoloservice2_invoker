f1ca150a0251ec4da890e8007cf42519487421a2
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-08-05 08:33:02 +0200

    [VERSION] Bump invoker to v1.8.6

--- app/worker_gpu.py
+++ app/worker_gpu.py
@@ -27,7 +27,7 @@ from states.run_training import RunTraining
 from wpipe.pipe import Pipeline
 
 # INVOKER VERSION
-VERSION = "v1.8.5"
+VERSION = "v1.8.6"
 PRIVATE_QUEUE = os.getenv("WORKER_HOST", "unknown")
 
 # Load local worker configuration
